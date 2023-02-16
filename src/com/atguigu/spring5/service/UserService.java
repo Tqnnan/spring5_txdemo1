f7047c0fb85f822b7f89c652f6a7d205b3e38862
nantan <457143894@qq.com>
2023-02-16 20:30:16 +0800

    声明事务管理

--- src/com/atguigu/spring5/service/UserService.java
+++ src/com/atguigu/spring5/service/UserService.java
@@ -3,6 +3,7 @@ package com.atguigu.spring5.service;
 import com.atguigu.spring5.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author 楠檀,
@@ -11,6 +12,10 @@ import org.springframework.stereotype.Service;
  */
 
 @Service
+@Transactional
+//@Transactional这个注解
+//添加到类上：指给这个类所以方法都添加事务
+//添加到方法上，指为这个方法添加事务
 public class UserService {
 
     @Autowired
@@ -18,6 +23,9 @@ public class UserService {
 
     public void accountMoney(){
         userDao.reduceMoney();
+
+        //模拟异常
+        int i = 10/0;
 
         userDao.addMoney();
     }
